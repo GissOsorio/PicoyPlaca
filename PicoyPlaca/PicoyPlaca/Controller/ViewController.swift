3580a146b1187b63c4c257d3bb167bc8f0575711
Gisela Osorio <giss_ot@hotmail.com>
2022-06-15 23:23:54 -0500

    View-Images

--- PicoyPlaca/PicoyPlaca/Controller/ViewController.swift
+++ PicoyPlaca/PicoyPlaca/Controller/ViewController.swift
@@ -9,11 +9,19 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var timeTxt: UITextField!
+    @IBOutlet weak var dateTxt: UITextField!
+    @IBOutlet weak var plateTxt: UITextField!
+    @IBOutlet weak var answerImage: UIImageView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
-
+    @IBAction func searchPlate(_ sender: UIButton) {
+        
+    }
+    
 }
 
