8aa96e631705b01654caafa7fe64d8128f7589b7
Gisela Osorio <giss_ot@hotmail.com>
2022-06-15 19:54:33 -0500

    Project Creation

---
+++ PicoyPlaca/PicoyPlaca/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  PicoyPlaca
+//
+//  Created by Gisela Osorio on 15/6/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
